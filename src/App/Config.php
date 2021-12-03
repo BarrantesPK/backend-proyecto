d0ac989ce70f3fee84f4280a6a3a23887ddc14ab
Axel0819 <64127962+Axel0819@users.noreply.github.com>
2021-12-02 22:51:43 -0600

    Update Config.php

--- src/App/Config.php
+++ src/App/Config.php
@@ -1,10 +1,10 @@
 <?php
 $container->set('config_bd', function() {
     return (object) [
-        "host" => "localhost",
-        "user" => "root",
-        "pass" => "",
-        "bd" => "prematric",
+        "host" => "sql5.freemysqlhosting.net",
+        "user" => "sql5451910",
+        "pass" => "WxVNwcRjAP",
+        "bd" => "sql5451910",
         "charset" => "utf8"
     ];
 });
